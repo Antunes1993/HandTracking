e518ab9c2451cb1e45bd1cce7b210221f8c9550f
Antunes <feoxp7@splm.siemens.com>
2021-12-30 13:31:13 -0300

    Modificações FingerCounting

--- FingerCounting.py
+++ FingerCounting.py
@@ -29,7 +29,7 @@ while(True):
     if len(lmList) != 0:
         fingers = []
         #Polegar
-        if lmList[tipIds[0]][1] < lmList[tipIds[0]-1][1]:
+        if lmList[tipIds[0]][1] > lmList[tipIds[0]-1][1]:
             fingers.append(1)
         else:
             fingers.append(0)
@@ -43,10 +43,32 @@ while(True):
 
         
         totalFingers = fingers.count(1)
-       
-        h, w, c = overlayList[totalFingers-1].shape     
-        frame[0:h, 0:w] = overlayList[totalFingers-1]         
+        print(totalFingers)
 
+        if totalFingers == 0:
+            h, w, c = overlayList[0].shape     
+            frame[0:h, 0:w] = overlayList[0]         
+
+        if totalFingers == 1:
+            h, w, c = overlayList[1].shape     
+            frame[0:h, 0:w] = overlayList[1]  
+
+        if totalFingers == 2:
+            h, w, c = overlayList[2].shape     
+            frame[0:h, 0:w] = overlayList[2]  
+
+        if totalFingers == 3:
+            h, w, c = overlayList[3].shape     
+            frame[0:h, 0:w] = overlayList[3]  
+    
+        if totalFingers == 4:
+            h, w, c = overlayList[4].shape     
+            frame[0:h, 0:w] = overlayList[4]  
+
+        if totalFingers == 5:
+            h, w, c = overlayList[5].shape     
+            frame[0:h, 0:w] = overlayList[5]  
+    
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
